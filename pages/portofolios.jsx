ddd0a32b01ff60f15ce1b648a6076e4e1bbda19f
billdelvin <billdelvin6@gmail.com>
2021-03-15 16:13:16 +0700

    creating list post

--- pages/portofolios.jsx
+++ pages/portofolios.jsx
@@ -1,7 +1,42 @@
 import { Component } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+import Layout from '../components/Layout';
 
 class Portofolios extends Component {
+ static async getInitialProps() {
+  let posts = [];
+  try {
+   const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+   posts = res.data;
+  } catch (error) {
+   console.log(error);
+  }
+
+  return { posts: posts.slice(0, 10) };
+ }
+
+ renderPosts(posts) {
+  return posts.map((post) => (
+   <li key={post.id}>
+    <Link href={'portofolios/[id]'} as={`portofolios/${post.id}`}>
+     <a> {post.title} </a>
+    </Link>
+   </li>
+  ));
+ }
+
  render() {
-  return <div></div>;
+  const { posts } = this.props;
+  return (
+   <Layout>
+    <h1>im Portofolios</h1>
+
+    <ul>{this.renderPosts(posts)}</ul>
+   </Layout>
+  );
  }
 }
+
+export default Portofolios;

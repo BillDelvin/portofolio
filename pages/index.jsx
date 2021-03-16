79b34eccf7802fe6719860183e23699fd379f0a6
billdelvin <billdelvin6@gmail.com>
2021-03-16 16:09:12 +0700

    adding button react strap

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
+import { Button } from 'reactstrap';
 
 export default function Home() {
  return (
   <Layout>
    <h1>im index</h1>
+   <Button color="danger">Danger!</Button>
   </Layout>
  );
 }

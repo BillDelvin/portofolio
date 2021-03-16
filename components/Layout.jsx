3b63ff5008a7dfb5773d5f47921d6380a0871e93
billdelvin <billdelvin6@gmail.com>
2021-03-16 16:06:58 +0700

    adding container

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,11 @@
 import Header from '../components/shared/Header';
+import { Container } from 'reactstrap';
 
 const Layout = ({ children }) => {
  return (
   <div>
    <Header />
-   {children}
+   <Container>{children}</Container>
   </div>
  );
 };

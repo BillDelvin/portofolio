c980e5b678b73735220d2dcb03df719c591df761
billdelvin <billdelvin6@gmail.com>
2021-03-17 09:04:35 +0700

    change to func component

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -1,50 +1,52 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import Link from 'next/link';
 
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
 
 const BsNavLink = ({ title, href }) => {
  return (
   <Link href={href}>
-   <a className="nav-link"> {title} </a>
+   <a className="nav-link port-navbar-link"> {title} </a>
   </Link>
  );
 };
 
-class Header extends Component {
- state = {
-  isOpen: false,
- };
+const BsNavBrand = () => (
+ <Link href="/">
+  <a className="navbar-brand port-navbar-brand">Bill Delvin</a>
+ </Link>
+);
 
- toggle = () => this.setState({ isOpen: !this.state.isOpen });
+const Header = () => {
+ const [isOpen, setIsOpen] = useState(false);
 
- render() {
-  return (
-   <Navbar color="light" light expand="md">
-    <NavbarBrand href="/">reactstrap</NavbarBrand>
-    <NavbarToggler onClick={this.toggle} />
-    <Collapse isOpen={this.state.isOpen} navbar>
-     <Nav className="mr-auto" navbar>
-      <NavItem>
-       <BsNavLink title="Home" href="/" />
-      </NavItem>
-      <NavItem>
-       <BsNavLink title="About" href="/about" />
-      </NavItem>
-      <NavItem>
-       <BsNavLink title="Portofolios" href="/portofolios" />
-      </NavItem>
-      <NavItem>
-       <BsNavLink title="Blog" href="/blog" />
-      </NavItem>
-      <NavItem>
-       <BsNavLink title="CV" href="/cv" />
-      </NavItem>
-     </Nav>
-    </Collapse>
-   </Navbar>
-  );
- }
-}
+ const toggle = () => setIsOpen(!isOpen);
+
+ return (
+  <Navbar className="port-navbar port-default absolute" color="transparent" dark expand="md">
+   <BsNavBrand />
+   <NavbarToggler onClick={toggle} />
+   <Collapse isOpen={isOpen} navbar>
+    <Nav className="mr-auto" navbar>
+     <NavItem className="port-navbar-item">
+      <BsNavLink title="Home" href="/" />
+     </NavItem>
+     <NavItem className="port-navbar-item">
+      <BsNavLink title="About" href="/about" />
+     </NavItem>
+     <NavItem className="port-navbar-item">
+      <BsNavLink title="Portofolios" href="/portofolios" />
+     </NavItem>
+     <NavItem className="port-navbar-item">
+      <BsNavLink title="Blog" href="/blog" />
+     </NavItem>
+     <NavItem className="port-navbar-item">
+      <BsNavLink title="CV" href="/cv" />
+     </NavItem>
+    </Nav>
+   </Collapse>
+  </Navbar>
+ );
+};
 
 export default Header;

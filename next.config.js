515eea0a7d7ed3e5ff6c8041e6328d5996f8cf8c
billdelvin <billdelvin6@gmail.com>
2021-03-18 08:48:03 +0700

    adding configuration

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
  sassOptions: {
   includePaths: [path.join(__dirname, 'styles')],
  },
+ webpack: (config) => {
+  config.resolve.alias['@'] = path.resolve(__dirname);
+  return config;
+ },
 };

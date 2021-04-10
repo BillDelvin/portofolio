fe98bb4154a73ed99fea02f7b4491c6f722a8434
billdelvin <billdelvin6@gmail.com>
2021-04-10 11:40:06 +0700

    adding comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss';
 
 const MyApp = ({ Component, pageProps }) => {
+ // console.log(process.env.AUTH0_DOMAIN);
  return <Component {...pageProps} />;
 };
 

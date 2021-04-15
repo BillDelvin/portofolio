848abf6afe187d78487f1314907d4e44afd65423
billdelvin <billdelvin6@gmail.com>
2021-04-15 11:12:16 +0700

    creating function validation for have a role

--- utils/auth0.jsx
+++ utils/auth0.jsx
@@ -14,6 +14,10 @@ const auth0 = initAuth0({
 
 export default auth0;
 
+export const isAuthorized = (user, role) => {
+ return user && user['http://localhost:3000/roles'].includes(role);
+};
+
 export const authorizeUser = async (req, res) => {
  const session = await auth0.getSession(req, res);
  if (!session || !session.user) {
@@ -25,4 +29,19 @@ export const authorizeUser = async (req, res) => {
  } else {
   return session.user;
  }
+};
+
+export const withAuth = (getData) => async ({ req, res }) => {
+ const session = await auth0.getSession(req, res);
+ if (!session || !session.user) {
+  res.writeHead(302, {
+   Location: '/api/v1/login',
+  });
+  res.end();
+  return { props: {} };
+ }
+
+ const data = getData ? await getData({ req, res }, session.user) : {};
+
+ return { props: { user: session.user, ...data } };
 };

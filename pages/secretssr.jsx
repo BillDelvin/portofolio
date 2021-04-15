b3f1f3c4cd878b98487919fbd97f9d39ff45b75f
billdelvin <billdelvin6@gmail.com>
2021-04-15 11:13:30 +0700

    still fixing it

--- pages/secretssr.jsx
+++ pages/secretssr.jsx
@@ -1,21 +1,35 @@
 import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
-import { authorizeUser } from '@/utils/auth0';
+import { withAuth } from '@/utils/auth0';
 
-const SecretSsr = ({ user }) => {
+const SecretSSR = ({ user, title }) => {
  return (
   <Layout user={user} loading={false}>
    <BasePage>
     <h1>Im secret page - hello {user && user.name}</h1>
+    <h2>{title}</h2>
    </BasePage>
   </Layout>
  );
 };
 
-export const getServerSideProps = async ({ req, res }) => {
- const user = await authorizeUser(req, res);
+// export const getServerSideProps = async ({ req, res }) => {
+//  const user = await authorizeUser(req, res);
 
- return { props: { user } };
+//  return { props: { user } };
+// };
+
+const getTitle = () => {
+ return new Promise((resolve) => {
+  setTimeout(() => {
+   resolve({ title: 'my new title!' });
+  }, 500);
+ });
 };
 
-export default SecretSsr;
+export const getServerSideProps = withAuth(async ({ req, res }, user) => {
+ const title = await getTitle();
+ return title;
+});
+
+export default SecretSSR;

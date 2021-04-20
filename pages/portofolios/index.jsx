70ef67a4f6962f98d44660fffdbfe136d453aa77
billdelvin <billdelvin6@gmail.com>
2021-04-20 18:13:48 +0700

    adding routing to detail page

--- pages/portofolios/index.jsx
+++ pages/portofolios/index.jsx
@@ -1,34 +1,45 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Row, Col } from 'reactstrap';
+import PortofolioCard from '@/components/PortofolioCard';
 import { useGetUser } from '@/actions/user';
-import { useGetData } from '@/actions';
+import PortofolioApi from '@/lib/api/portofolio';
 
 import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
 
-const Portofolios = () => {
- const { data, error, loading } = useGetData();
+const Portofolios = ({ portofolios }) => {
+ const router = useRouter();
  const { data: dataUser, loading: loadingUser } = useGetUser();
-
- const renderPosts = (posts) => {
-  return posts.map((post) => (
-   <li key={post.id}>
-    <Link as={`/portofolios/${post.id}`} href="/portofolios/[id]">
-     <a> {post.title} </a>
-    </Link>
-   </li>
-  ));
- };
 
  return (
   <Layout user={dataUser} loading={loadingUser}>
-   <BasePage>
-    <h1>im Portofolios</h1>
-    {loading && <p>Loading Data....</p>}
-    {data && <ul>{renderPosts(data)}</ul>}
-    {error && <div className="alert alert-danger">{error.message}</div>}
+   <BasePage header="Portofolios" className="portfolio-page">
+    <Row md="3">
+     {portofolios.map((portofolio) => (
+      <Col
+       key={portofolio._id}
+       onClick={() => {
+        router.push('/portofolios/[id]', `/portofolios/${portofolio._id}`);
+       }}
+      >
+       <PortofolioCard portofolio={portofolio} />
+      </Col>
+     ))}
+    </Row>
    </BasePage>
   </Layout>
  );
 };
+
+// this function is called during the build time
+// improved performance of page
+// it will create static page with dynamic data
+export async function getStaticProps() {
+ const getData = await new PortofolioApi().getAll();
+ const portofolios = getData.data;
+ return {
+  props: { portofolios },
+ };
+}
 
 export default Portofolios;

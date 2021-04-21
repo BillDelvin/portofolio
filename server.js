291c416b667fc9aacbe47aa3485dea7f75af56fc
billdelvin <billdelvin6@gmail.com>
2021-04-21 16:52:55 +0700

    change port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.prepare().then(() => {
  });
 
  const PORT = process.env.NODE_ENV || 3000;
- server.listen(PORT, (err) => {
+ server.listen(3000, (err) => {
   if (err) console.log(err);
   console.log(`> Listen on port ${PORT}`);
  });

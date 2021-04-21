09eaca096818b262815f2b2e7ebfd2e090a312c9
billdelvin <billdelvin6@gmail.com>
2021-04-21 11:34:32 +0700

    change content

--- pages/portofolios/[id].jsx
+++ pages/portofolios/[id].jsx
@@ -2,29 +2,45 @@ import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
 import { useRouter } from 'next/router';
 import { useGetUser } from '@/actions/user';
-import { useGetDataById } from '@/actions';
+import PortofolioApi from '../../lib/api/portofolio';
 
-const PortofoliosDetail = () => {
+const PortofoliosDetail = ({ portofolio }) => {
  const router = useRouter();
- const { data: post, error, loading } = useGetDataById(router.query.id);
  const { data: dataUser, loading: loadingUser } = useGetUser();
 
  return (
   <Layout user={dataUser} loading={loadingUser}>
-   <BasePage>
-    {loading && <p>Loading Data....</p>}
-    {post && (
-     <div>
-      <h1>Im portofolio detail</h1>
-      <h1>{post.title}</h1>
-      <p>body : {post.body}</p>
-      <p>id : {post.id}</p>
-     </div>
-    )}
-    {error && <div className="alert alert-danger">{error.message}</div>}
-   </BasePage>
+   <BasePage header="Portofolio Detail">{JSON.stringify(portofolio)}</BasePage>
   </Layout>
  );
 };
+
+// export async function getServerSideProps({ query }) {
+//  const getData = await new PortofolioApi().getById(query.id);
+//  const portofolio = getData.data;
+//  return { props: { portofolio } };
+// }
+
+// this function execute at the build time
+export async function getStaticPaths() {
+ const getData = await new PortofolioApi().getAll();
+ const portofolios = getData.data;
+
+ // get the paths we want pre-render based on portofolio ID
+ const paths = portofolios.map((portofolio) => {
+  return {
+   params: { id: portofolio._id },
+  };
+ });
+
+ // fallback: false mean that not found pages will be resolved to 404 page
+ return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+ const getData = await new PortofolioApi().getById(params.id);
+ const portofolio = getData.data;
+ return { props: { portofolio } };
+}
 
 export default PortofoliosDetail;

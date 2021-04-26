cb6a377fa244f5cd95e5c5cba0a631b2914ce5ba
billdelvin <billdelvin6@gmail.com>
2021-04-26 21:29:56 +0700

    creating delete controller and edit button

--- pages/portofolios/index.jsx
+++ pages/portofolios/index.jsx
@@ -1,15 +1,29 @@
-import { useRouter } from 'next/router';
-import { Row, Col } from 'reactstrap';
-import PortofolioCard from '@/components/PortofolioCard';
-import { useGetUser } from '@/actions/user';
-import PortofolioApi from '@/lib/api/portofolio';
-
+import { useState } from 'react';
 import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
+import { useRouter } from 'next/router';
+import { Row, Col, Button } from 'reactstrap';
+import PortofolioCard from '@/components/PortofolioCard';
+import { useGetUser } from '@/actions/user';
+import { isAuthorized } from '../../utils/auth0';
+import PortofolioApi from '@/lib/api/portofolio';
+import { useDeletePortofolio } from '../../actions/portofolio';
 
-const Portofolios = ({ portofolios }) => {
+const Portofolios = ({ portofolios: initialPortofolios }) => {
  const router = useRouter();
+ const [portofolios, setPortofolios] = useState(initialPortofolios);
  const { data: dataUser, loading: loadingUser } = useGetUser();
+ const [deletePortofolio, { data, error }] = useDeletePortofolio();
+
+ const _deletePortofolio = async (e, id) => {
+  e.stopPropagation();
+  const isConfirm = confirm('Are you sure want to delete this portofolios?');
+  if (isConfirm) {
+   await deletePortofolio(id);
+   const newPortofolios = portofolios.filter((portofolio) => portofolio._id !== id);
+   setPortofolios(newPortofolios);
+  }
+ };
 
  return (
   <Layout user={dataUser} loading={loadingUser}>
@@ -22,7 +36,25 @@ const Portofolios = ({ portofolios }) => {
         router.push('/portofolios/[id]', `/portofolios/${portofolio._id}`);
        }}
       >
-       <PortofolioCard portofolio={portofolio} />
+       <PortofolioCard portofolio={portofolio}>
+        {dataUser && isAuthorized(dataUser, 'admin') && (
+         <>
+          <Button
+           className="mr-3"
+           color="warning"
+           onClick={(e) => {
+            e.stopPropagation();
+            router.push('/portofolios/[id]/edit', `/portofolios/${portofolio._id}/edit`);
+           }}
+          >
+           Edit
+          </Button>
+          <Button color="danger" onClick={(e) => _deletePortofolio(e, portofolio._id)}>
+           Delete
+          </Button>
+         </>
+        )}
+       </PortofolioCard>
       </Col>
      ))}
     </Row>

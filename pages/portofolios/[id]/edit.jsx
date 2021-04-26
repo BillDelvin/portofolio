fabf6d9052c7f5125545659b9984306e4cb0ba16
billdelvin <billdelvin6@gmail.com>
2021-04-26 19:49:24 +0700

    creating form for update

--- pages/portofolios/[id]/edit.jsx
+++ pages/portofolios/[id]/edit.jsx
@@ -2,21 +2,42 @@ import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
 import withAuth from '../../../hoc/withAuth';
-import { useGetPortofolio } from '../../../actions/portofolio';
+import { useGetPortofolio, useUpdatedPortofolio } from '../../../actions/portofolio';
 import PortofolioForm from '@/components/PortofolioForm';
 import { Row, Col } from 'reactstrap';
+import { toast } from 'react-toastify';
 
 const PortofolioEdit = ({ user }) => {
  const router = useRouter();
- const { data } = useGetPortofolio(router.query.id);
+ const [updatedPortofolio, { error }] = useUpdatedPortofolio();
+ const { data: initialData } = useGetPortofolio(router.query.id);
+ const _updatedPortofolio = async (data) => {
+  // try {
+  //  await updatedPortofolio(router.query.id, data);
+  //  toast.success('Portofolio have been updated!', { autoClose: 3000 });
+  // } catch (error) {
+  //  toast.error('Oops some error!', { autoClose: 3000 });
+  // }
+  await updatedPortofolio(router.query.id, data)
+   .then(() => {
+    toast.success('Portofolio have been updated!', { autoClose: 3000 });
+   })
+   .catch(() => {
+    toast.error('Oops some error!', { autoClose: 3000 });
+   });
+ };
  return (
   <Layout user={user} loading={false}>
    <BasePage header="Portofolio Edit">
     <Row>
      <Col md="8">
-      {data && (
-       <PortofolioForm initalUpdateData={data} onSubmit={(data) => alert(JSON.stringify(data))} />
+      {initialData && (
+       <PortofolioForm
+        initalUpdateData={initialData}
+        onSubmit={(data) => _updatedPortofolio(data)}
+       />
       )}
+      {error && <div className="alert alert-danger mt-2">{error}</div>}
      </Col>
     </Row>
    </BasePage>

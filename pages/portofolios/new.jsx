84b7196862031974746c618cdac90ecb629dcac1
billdelvin <billdelvin6@gmail.com>
2021-04-26 11:36:45 +0700

    make redirect after creating data

--- pages/portofolios/new.jsx
+++ pages/portofolios/new.jsx
@@ -3,19 +3,23 @@ import BasePage from '@/components/BasePage';
 import PortofolioForm from '@/components/PortofolioForm';
 import withAuth from '../../hoc/withAuth';
 import { Row, Col } from 'reactstrap';
-import { createPortofolio } from '@/actions/portofolio';
+import { useCreatePortofolio } from '@/actions/portofolio';
+import Redirect from '../../components/shared/Redirect';
 
 const PortofolioNew = ({ userData, userLoading }) => {
- const _createPortofolio = (data) => {
-  createPortofolio(data);
- };
+ const [createPortofolio, { data, loading, error }] = useCreatePortofolio();
+
+ if (data) {
+  return <Redirect to="/portofolios" />;
+ }
 
  return (
   <Layout user={userData} loading={userLoading}>
    <BasePage header="Create Portofolio">
     <Row>
      <Col md="8">
-      <PortofolioForm onSubmit={_createPortofolio} />
+      <PortofolioForm onSubmit={createPortofolio} />
+      {error && <div className="alert alert-danger mt-2">{error}</div>}
      </Col>
     </Row>
    </BasePage>

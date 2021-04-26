67f9129b83d22c2787986ec820c86448df500b08
billdelvin <billdelvin6@gmail.com>
2021-04-26 11:36:06 +0700

    make request send with token

--- pages/api/v1/portofolios.jsx
+++ pages/api/v1/portofolios.jsx
@@ -1,11 +1,12 @@
 import PortofolioApi from '../../../lib/api/portofolio';
+import auth0 from '../../../utils/auth0';
 
 export default async function createPortofolio(req, res) {
  try {
-  const data = req.body;
-  await new PortofolioApi().createPortofolio(data);
-  return res.status(200).json({ message: 'Portofolio was created!' });
+  const { accessToken } = await auth0.getAccessToken(req, res);
+  const createPortofolio = await new PortofolioApi(accessToken).createPortofolio(req.body);
+  return res.status(200).json(createPortofolio.data);
  } catch (error) {
-  return res.status(error.status || 400).end(error.message);
+  return res.status(error.status || 422).json(error.response.data);
  }
 }

94f2d009e3468ca9dc4434acdadc5311305efc42
billdelvin <billdelvin6@gmail.com>
2021-04-26 14:19:16 +0700

    creating controller for get profile by id

--- actions/portofolio.jsx
+++ actions/portofolio.jsx
@@ -1,5 +1,12 @@
 import axios from 'axios';
 import { useApiHandler } from './index';
+import useSWR from 'swr';
+import { fetcher } from './index';
 
 export const useCreatePortofolio = () =>
  useApiHandler((data) => axios.post('/api/v1/portofolios', data));
+
+export const useGetPortofolio = (id) => {
+ const { data, error, ...rest } = useSWR(`/api/v1/portofolios/${id}`, fetcher);
+ return { data, error, loading: !data && !error, ...rest };
+};

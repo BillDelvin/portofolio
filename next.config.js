6e94e1b501d22879af40f1b808183b9405ed44a8
billdelvin <billdelvin6@gmail.com>
2021-04-27 14:07:53 +0700

    setting few configuration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
  sassOptions: {
   includePaths: [path.join(__dirname, 'styles')],
- },
- webpack: (config) => {
-  config.resolve.alias['@'] = path.resolve(__dirname);
-  config.plugins.push(new Dotenv({ silent: true }));
-  return config;
  },
  env: {
   AUTH0_NAMEPSPACE: 'http://localhost:3000/roles',

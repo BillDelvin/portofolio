b3e43fceee4dfd5595d65336a8b499c8da0cb99a
billdelvin <billdelvin6@gmail.com>
2021-04-28 19:07:46 +0700

    adding editor component

--- pages/blogs/editor/index.jsx
+++ pages/blogs/editor/index.jsx
@@ -1,11 +1,18 @@
 import Layout from '@/components/Layout';
 import BasePage from '@/components/BasePage';
 import withAuth from '../../../hoc/withAuth';
+import { Editor } from 'slate-simple-editor';
 
 const BlogEditor = ({ user, loading }) => {
+ const saveBlog = (data) => {
+  console.log(data);
+ };
+
  return (
   <Layout user={user} loading={loading}>
-   <BasePage header="Blog Editor"></BasePage>
+   <BasePage>
+    <Editor onSave={saveBlog} />
+   </BasePage>
   </Layout>
  );
 };
